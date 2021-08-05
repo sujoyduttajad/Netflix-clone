78ce490f2b24f7508c3346d6391c8a36194e0827
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-05 18:03:50 +0530

    Created different pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { FaqsContainer } from './containers/faqs';
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
 import * as ROUTES from './constants/routes'
+import { Home, Browse, Signin, Signup } from './pages';
 
 export default function App() {
   return ( 
     <Router>
       <Route exact path={ROUTES.HOME} >
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <Home />
       </Route>
     </Router> 
   )
