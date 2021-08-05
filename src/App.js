ee24b69d4f138193bd773d8b7af2e9ad0e36cd78
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-05 17:43:10 +0530

    Setting up react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router';
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
-
+import * as ROUTES from './constants/routes'
 
 export default function App() {
   return ( 
-    <>
+    <Router>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
-    </> 
+    </Router> 
   )
 }
