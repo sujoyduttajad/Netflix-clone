6aea34fbf7f0aca7f488879271065a46e80ffffc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-06 12:44:30 +0530

    Created Signup and added some routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import { Home, Browse, Signin, Signup } from './pages';
 export default function App() {
   return ( 
     <Router>
+      <Route exact path='/browse' >
+        <Browse />
+      </Route>
+      <Route exact path='/signup' >
+        <Signup />
+      </Route>
+      <Route exact path='/signin' >
+        <Signin />
+      </Route>
       <Route exact path={ROUTES.HOME} >
         <Home />
       </Route>
---
+++ src/pages/signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function signup() {
+    return (
+        <div>
+            <h1>Hello from the signup</h1>
+        </div>
+    )
+}
+
+export default signup
