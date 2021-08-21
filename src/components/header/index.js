39e0105a371ebc75d89a707737a5f46708acd59e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-21 18:17:38 +0530

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -12,7 +12,7 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
 }
 
-Header.ButtonLink = function ButtonLink({ children, ...restProps }) {
+Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
     return <ButtonLink {...restProps}>{children}</ButtonLink>;
 }
 
