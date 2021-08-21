11433997d5d79527956ac469a08dafed60dc14f0
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-21 17:42:54 +0530

    updated the export statements

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from '../components/accordion/styles/accordion'
+import { Header } from '../components/header/styles/header'
 
 function HeaderContainer() {
     return (
