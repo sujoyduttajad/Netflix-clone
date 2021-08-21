5c46369ca3e95eaac89592ee37e2729d02f63987
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-21 20:15:30 +0530

    Added OptForm as a reusable component

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,15 +3,26 @@ import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
+import { OptForm } from '../components';
 
 export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <JumbotronContainer />
-                <FaqsContainer />
-                <FooterContainer />  
-            </HeaderContainer>
+                <OptForm>
+                    <OptForm.Input placeholder="Email address" />
+                    <OptForm.Button>GET STARTED</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>
+                        Ready to watch? Enter your email to create or restart your 
+                        membership.
+                    </OptForm.Text>
+                </OptForm>
+            </HeaderContainer>    
+            <JumbotronContainer />
+            <FaqsContainer />
+            <FooterContainer />  
+            
         </>
     )
 }
