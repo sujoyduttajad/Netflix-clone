f84206ad6cdfcd1c15741a5560927eb7fabb67b5
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-21 17:35:10 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 
 export default function Home() {
     return (
         <>
-            <JumbotronContainer />
-            <FaqsContainer />
-            <FooterContainer />  
+            <HeaderContainer>
+                <JumbotronContainer />
+                <FaqsContainer />
+                <FooterContainer />  
+            </HeaderContainer>
         </>
     )
 }
