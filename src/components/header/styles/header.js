36ef6892adda99e87f4d453492b4c0d911a9b1ae
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-21 19:57:30 +0530

    added dynamic image styles

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components/macro';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
-export const Background = styled.div``;
+export const Background = styled.div`
+    display: flex;
+    flex-direction: column;
+    background: url(${({ src }) => 
+        src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg'}) top left / cover no-repeat;
+`;
 
 export const Frame = styled.div``;
 
