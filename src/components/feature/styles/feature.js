49a7e7d50520ab936f1bc149b2a7f06aec1295da
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-24 06:16:33 +0530

    Update feature.js

--- src/components/feature/styles/feature.js
+++ src/components/feature/styles/feature.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-bottom: 8px solid solid #222;
+    text-align: center;
+    padding: 165px 45px;
+`;
 
 export const Title = styled.h1`
     color: white;
