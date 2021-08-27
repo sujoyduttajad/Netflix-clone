226cf3cc0d6a3366e4b228558d427087d9feb663
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-27 21:32:37 +0530

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -2,3 +2,11 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+// we need to somehoe seed the database
+
+// we need a config here
+const config = {};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
