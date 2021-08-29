b18cc49b9cfbd4430ce452102691f7dd0d849015
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-29 16:00:49 +0530

    Created FirebaseContext

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import 'normalize.css';
 import { GlobalStyles } from './global-styles';
 import { firebase } from './lib/firebase.prod';
 
-render( <>
-    <React.StrictMode>
-        <FirebaseContext.Provider value={{ firebase }}>
-            <GlobalStyles />
-            <App />
-        </FirebaseContext.Provider>
-    </React.StrictMode>
+render( 
+    <>
+        <React.StrictMode>
+            <FirebaseContext.Provider value={{ firebase }}>
+                <GlobalStyles />
+                <App />
+            </FirebaseContext.Provider>
+        </React.StrictMode>
     </> 
     ,document.getElementById('root'))
 
