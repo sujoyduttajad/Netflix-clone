404ea2b2416c7d757736a82814b4ce617f0a90cc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-30 23:15:56 +0530

    Update signin.js

---
+++ src/pages/signin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeaderContainer } from '../containers/header';
+
+const signin = () => {
+    return (
+        <div>
+            <p>Hello from the sign in!!</p>
+        </div>
+    )
+}
+
+export default signin
