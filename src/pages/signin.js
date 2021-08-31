3d7f78006ed96c6a8ad9135fbddb9b74ab395244
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-31 13:39:08 +0530

    Added form component and initialized states in form.js

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
+import { Form } from '../components';
 
-const signin = () => {
+const Signin = () => {
+    const [emailAddress, setEmailAddress] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState('');
+
+
     return (
         <>
             <HeaderContainer>HELLO from sign in</HeaderContainer>
@@ -11,4 +17,4 @@ const signin = () => {
     )
 }
 
-export default signin
+export default Signin
