7972dd8d2d0ccea8f8dd141d9b5415b51004a4a9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-05 17:34:25 +0530

    Added styles to the Container

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 import { Link as ReachRouterLink } from 'react-router-dom';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 660px;
+    background-color: rgba(0, 0, 0, 0.75);
+    border-radius: 5px;
+    box-sizing: border-box;
+    width: 100%;
+    max-width: 450px;
+    padding: 60px 68px 40px;
+    margin-bottom: 100px;
+`;
 
 export const Error = styled.div``;
 
