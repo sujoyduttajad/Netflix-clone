e23033f1537792f9eaef905196cf4bbe2c699421
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-11 18:08:17 +0530

    Added the button styles

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -74,4 +74,18 @@ export const Input = styled.input`
     }
 `;
 
-export const Submit = styled.button``;
+export const Submit = styled.button`
+    background: #e50914;
+    border-radius: 4px;
+    font-size: 16px;
+    font-weight: bold;
+    margin: 24px 0 12px;
+    padding: 16px;
+    border: 0;
+    color: white;
+    cursor: pointer;
+
+    &:disabled {
+        opacity: 0.5;
+    }
+`;
