775362b736b3e2e5f6c4f8eb4ec89c6bf84321c7
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-11-20 19:48:03 +0530

    Update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
+import { Form } from '../components';
+import { FirebaseContext } from '../context/firebase';
+import * as ROUTES from '../constants/routes'; 
+// useHistory allows users to push into different pageson an action
+import { useHistory } from 'react-router';
 
-function signup() {
+
+function Signup() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [firstName, setFirstName] = useState('');
+
     return (
         <div>
             <h1>Hello from the signup</h1>
@@ -8,4 +21,4 @@ function signup() {
     )
 }
 
-export default signup
+export default Signup
