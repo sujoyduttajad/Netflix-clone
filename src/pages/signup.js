15b821c03ae88836ac0bd7919985d3eb8996d874
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-26 22:44:07 +0530

    Update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -71,8 +71,8 @@ function Signup() {
                     </Form.Submit>
 
                     <Form.Text>
-                        Already a user? 
-                        <Form.Link to="/signin">Sign in now.</Form.Link>
+                        Already a user?
+                        <Form.Link to="/signin"> Sign in now.</Form.Link>
                     </Form.Text>
                     <Form.TextSmall>
                         This page is protected by Google reCAPTCHA to ensure you're not a bot.
