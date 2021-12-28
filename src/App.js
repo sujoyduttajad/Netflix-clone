d6070f188ca323c81f1459d3b98dd3d2b3329362
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-28 21:17:34 +0530

    Added the IsUserRedirect component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes'
 import { Home, Browse, Signin, Signup } from './pages';
+import { IsUserRedirect } from './helpers/routes' 
 
 export default function App() {
+  const user = {};
+
   return ( 
     <Router>
-      <Route exact path='/browse' >
-        <Browse />
-      </Route>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
+        <Signin />
+      </IsUserRedirect>       
       <Route exact path='/signup' >
         <Signup />
       </Route>
-      <Route exact path='/signin' >
-        <Signin />
+      <Route exact path='/browse' >
+        <Browse />
       </Route>
+      
       <Route exact path={ROUTES.HOME} >
         <Home />
       </Route>
