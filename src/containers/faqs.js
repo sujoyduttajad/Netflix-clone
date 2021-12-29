349bf0d443b33a3314b32c2502e75ce4f0e6a18f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-29 16:28:41 +0530

    Update faqs.js

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -16,13 +16,13 @@ export function FaqsContainer() {
             <Accordion.Item />
 
             <OptForm>
-                <OptForm.Input placeholder="Email address" />
-                <OptForm.Button>GET STARTED</OptForm.Button>
-                <OptForm.Break />
                 <OptForm.Text>
                     Ready to watch? Enter your email to create or restart your 
                     membership.
                 </OptForm.Text>
+                <OptForm.Input placeholder="Email address" />
+                <OptForm.Button>Get Started</OptForm.Button>
+                <OptForm.Break />            
             </OptForm>
         </Accordion>
     )
