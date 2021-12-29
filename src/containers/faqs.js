d4186bbefcd2be7ef9a7b2e3e654b99532d83912
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-29 16:40:45 +0530

    Update faqs.js

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -13,7 +13,7 @@ export function FaqsContainer() {
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
-            <Accordion.Item />
+            {/* <Accordion.Item /> */}
 
             <OptForm>
                 <OptForm.Text>
