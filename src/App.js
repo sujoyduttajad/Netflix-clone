3695e8e16e10ec3883593023aabc91629b390556
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-01-04 19:25:22 +0530

    Update App.js with Switch and new user

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes'
 import { Home, Browse, Signin, Signup } from './pages';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes' 
 
 export default function App() {
-  const user = {};
+  // const user = null;
+  const user = { name: 'Sujoy' };
 
   return ( 
     <Router>
-      <IsUserRedirect
-        user={user}
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.SIGN_IN}
-        exact
-      >
-        <Signin />
-      </IsUserRedirect>       
-      <IsUserRedirect
-        user={user}
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.SIGN_UP}
-        exact
-      >
-        <Signup />
-      </IsUserRedirect>     
-      <ProtectedRoute
-        user={user}
-        path={ROUTES.BROWSE}
-        exact
-      >
-        <Browse />
-      </ProtectedRoute>
-      <IsUserRedirect 
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.HOME} 
-        exact 
-      >
-        <Home />
-      </IsUserRedirect>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+          exact
+        >
+          <Signin />
+        </IsUserRedirect>       
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+          exact
+        >
+          <Signup />
+        </IsUserRedirect>     
+        <ProtectedRoute
+          user={user}
+          path={ROUTES.BROWSE}
+          exact
+        >
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect 
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME} 
+          exact 
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router> 
   )
 }
