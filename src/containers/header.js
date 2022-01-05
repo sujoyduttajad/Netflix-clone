987130676d016e1da123e7e5b5937596d27d03e9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-01-05 23:56:28 +0530

    Update header.js

--- src/containers/header.js
+++ src/containers/header.js
@@ -6,11 +6,13 @@ import logo from '../logos/logo.svg'
 export function HeaderContainer({ children }) {
     return (
         <Header>
-            <Header.Frame>
-                <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
-                <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
-            </Header.Frame>
-            {children}
+            <Header.Image />
+                <Header.Frame>
+                    <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+                    <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+                </Header.Frame>            
+                    {children}
+            {/* </Header.Image> */}
         </Header>
     )
 }
