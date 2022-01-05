15b0db7413010a6ca19b874988a63b9edf72840c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-01-05 23:56:31 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,18 +10,18 @@ export default function Home() {
         <>
             <HeaderContainer>
                 <Feature>
-                    <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                    <Feature.Title>Unlimited movies, TV shows and more.</Feature.Title>
                     <Feature.SubTitle>
                         Watch anywhere. Cancel at any time.
                     </Feature.SubTitle>
                     <OptForm>
-                        <OptForm.Input placeholder="Email address" />
-                        <OptForm.Button>Get Started</OptForm.Button>
-                        <OptForm.Break />
                         <OptForm.Text>
                             Ready to watch? Enter your email to create or restart your 
                             membership.
                         </OptForm.Text>
+                        <OptForm.Input placeholder="Email address" />
+                        <OptForm.Button>Get Started</OptForm.Button>
+                        <OptForm.Break />                       
                     </OptForm>
                 </Feature>      
             </HeaderContainer>    
