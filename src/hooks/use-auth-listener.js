0b9f4ac32c5bf55708a048e90c9fe05bcd48b1ef
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-01-06 18:17:01 +0530

    Update use-auth-listener.js and set the localStorage with authUser

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -2,7 +2,19 @@ import React, {useState, useEffect, useContext} from 'react'
 import { FirebaseContext } from '../context/firebase'
 
 const useAuthListener = () => {
-    
+    const [user, setUser] = useState(
+        JSON.parse(localStorage.getItem('authUser'))
+    );
+    const { firebase } = useContext(FirebaseContext); 
+
+    useEffect(() => {
+        const listener = firebase.auth().onAuthStateChanged((authUser) => {
+            if(authUser) {
+                localStorage.setItem('authUser', JSON.stringify(authUser));
+                setUser(authUser);
+            }
+        })
+    }, [])
 }
 
 export default useAuthListener
