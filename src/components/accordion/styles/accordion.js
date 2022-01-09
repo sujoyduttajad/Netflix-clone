3daf23aabd5c37c39e044b7eb0507b172bdacd4c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-01-09 18:23:04 +0530

    Update accordion.js

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -23,6 +23,10 @@ export const Item = styled.div`
     &:first-of-type {
         margin-top: 3em;
     }
+
+    &:last-of-type {
+        margin-bottom: 0;
+    }
 `;
 
 export const Header = styled.div`
@@ -43,6 +47,7 @@ export const Header = styled.div`
         width: 20em;
         height: 20em;
         font-size: 2px;
+        user-select: none;
 
         @media (max-width: 600px) {
             width: 16px;
@@ -59,11 +64,25 @@ export const Body = styled.div`
     transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
     font-size: 26px;
     font-weight: normal;
-    font-height: normal;
+    line-height: normal;
     background: #303030;
-    padding: 0.8em 2.2em 0.8em 1.2em;
     white-space: pre-wrap;
     user-select: none;
+    overflow: hidden;
+
+    &.closed {
+        max-height: 0;
+        overflow: hidden;
+        transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
+    }
+    &.open {
+        max-height: 1200px;
+        transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
+    }
+    span {
+        display: block;
+        padding: 0.8em 2.2em 0.8em 1.2em;
+    }
 
     @media (max-width: 600px) {
         font-size: 16px;
