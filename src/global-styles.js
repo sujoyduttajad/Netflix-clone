ed0789041efb1ffef3e96ee2b474ad8ac3fe2ac6
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-01-09 18:23:07 +0530

    Update global-styles.js

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,11 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
     html, body {
+        height: 100%;
         font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: greyscale;
-        background-color: black;
+        background-color: #000;
         color: #333333;
         font-size: 16px;
     }
