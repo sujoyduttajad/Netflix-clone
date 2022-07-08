8854e10e6c254ff6088d5f950a239d521107cb7c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-07-08 17:03:45 +0530

    Update browse.js

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useContent } from '../hooks'
 
 const Browse = () => {
+
+    // we need the series and the films 
+    const { series } = useContent('series');
+    console.log(series);
+    // we need slides
+    // pass it to he browse container
+    
     return (
         <div>
             <h1>Hello from the Browse page</h1>
