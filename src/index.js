76b40d7ec9fbc6c82f6f69db030152194a49dbc0
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-28 16:57:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App.js';
-import 'normalize.css';
-import { GlobalStyles } from './global-styles';
-import { firebase } from './lib/firebase.prod';
-import { FirebaseContext } from './context/firebase'; 
+import React from "react";
+import { render } from "react-dom";
+import App from "./App.js";
+import "normalize.css";
+import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
-render( 
-    <>
-        <React.StrictMode>
-            <FirebaseContext.Provider value={{ firebase }}>
-                <GlobalStyles />
-                <App />
-            </FirebaseContext.Provider>
-        </React.StrictMode>
-    </> 
-    ,document.getElementById('root'))
-
+render(
+  <React.StrictMode>
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
