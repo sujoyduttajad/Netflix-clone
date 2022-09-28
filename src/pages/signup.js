814cd94fdd7e8b36d49f74e575c9f7991b5da3f4
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-28 16:57:46 +0530

    Update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,92 +1,93 @@
-import React, {useState, useContext} from 'react'
-import { FooterContainer } from '../containers/footer';
-import { HeaderContainer } from '../containers/header';
-import { Form } from '../components';
-import { FirebaseContext } from '../context/firebase';
-import * as ROUTES from '../constants/routes'; 
+import React, { useState, useContext } from "react";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import { FirebaseContext } from "../context/firebase";
+import * as ROUTES from "../constants/routes";
 // useHistory allows users to push into different pages on an action
-import { useHistory } from 'react-router';
-
+import { useHistory } from "react-router";
 
 function Signup() {
-    const history = useHistory();
-    const { firebase } = useContext(FirebaseContext);
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
-    const [firstName, setFirstName] = useState('');
-    const [emailAddress, setEmailAddress] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const isInvalid = firstName === '' ||  password === '' || emailAddress === '';
+  const isInvalid = firstName === "" || password === "" || emailAddress === "";
 
-    const handleSignUp = (event) => {
-        event.preventDefault();
+  const handleSignUp = (event) => {
+    event.preventDefault();
 
-        // firebase auth(this is a Promise)
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(emailAddress, password)
-            .then((result) => 
-                result.user.updateProfile({ 
-                    displayName: firstName,
-                    photoURL: Math.floor(Math.random() * 5) + 1,
-                 }).then(() => {
-                     history.push(ROUTES.BROWSE);
-                 })
-            )
-            .catch((error) => {
-                setFirstName('');
-                setEmailAddress('');
-                setPassword('');
-                setError(error.message);
-            }); 
-    };
+    // firebase auth(this is a Promise)
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          })
+      )
+      .catch((error) => {
+        setFirstName("");
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
 
-    return (
-        <>
-        <HeaderContainer>
-            <Form>
-                <Form.Title>Sign Up</Form.Title>
-                {error && <Form.Error>{error}</Form.Error>}
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
 
-                <Form.Base onSubmit={handleSignUp} method="POST">
-                    <Form.Input
-                        placeholder="First name"
-                        value={firstName}
-                        onChange={({ target }) => setFirstName(target.value)}  
-                    />
-                    <Form.Input
-                        type="email"
-                        placeholder="Email address"
-                        value={emailAddress}
-                        onChange={({ target }) => setEmailAddress(target.value)}  
-                    />
-                    <Form.Input
-                        type="password"
-                        value={password}
-                        autoComplete="off"
-                        placeholder="Password"
-                        onChange={({ target }) => setPassword(target.value)}  
-                    />
-                    <Form.Submit disabled={isInvalid} type="submit">
-                        Sign Up
-                    </Form.Submit>
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              placeholder="First name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              type="email"
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
 
-                    <Form.Text>
-                        Already a user?
-                        <Form.Link to="/signin"> Sign in now.</Form.Link>
-                    </Form.Text>
-                    <Form.TextSmall>
-                        This page is protected by Google reCAPTCHA to ensure you're not a bot.
-                        <Form.Link to="#" > Learn more.</Form.Link>
-                    </Form.TextSmall>
-
-                </Form.Base>
-            </Form>
-        </HeaderContainer>
-        <FooterContainer />
-        </>
-    )
+            <Form.Text>
+              Already a user?
+              <Form.Link to="/signin"> Sign in now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot.
+              <Form.Link to="#"> Learn more.</Form.Link>
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
 
-export default Signup
+export default Signup;
