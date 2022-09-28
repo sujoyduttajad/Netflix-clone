3e0f91cdbdcfe000a6e3595ea419ae446aae8f59
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-28 17:30:09 +0530

    Update routes.js

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,55 +1,50 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-export function IsUserRedirect({ user, loggedInPath, children, ...rest}) {
-    return (
-        <Route 
-            {...rest} 
-            render={() => {
-                if(!user) {
-                    return children;
-                }
-                if(user) {
-                    return (
-                        <Redirect 
-                            to={{
-                                pathname: loggedInPath
-                            }}
-                        />
-                    )
-                }
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
 
-                return null;
-             }}
-        />
-    )
+        return null;
+      }}
+    />
+  );
 }
 
 export function ProtectedRoute({ user, children, ...rest }) {
-    return (
-
-        <Route 
-            {...rest}
-            render={({ location }) => {
-                if(user) {
-                    return children;
-                }
-                
-                if(!user) {
-                    return (
-                        <Redirect 
-                            to={{
-                                pathname: 'signin',
-                                state: { from: location },
-                            }}
-                        />
-                    )
-                }
-
-                return null;
-            }}
-        />
-    )
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: "signin",
+                state: { from: location },
+              }}
+            />
+          );
+        }
+        return null;
+      }}
+    />
+  );
 }
-
-
