3502e12789a9298d037cd766326b99c6ad0acd4b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-28 16:57:49 +0530

    Update signin.js

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,77 +1,77 @@
-import React, {useState, useContext} from 'react'
-import { FooterContainer } from '../containers/footer';
-import { HeaderContainer } from '../containers/header';
-import { Form } from '../components';
-import { FirebaseContext } from '../context/firebase';
-import * as ROUTES from '../constants/routes'; 
+import React, { useState, useContext } from "react";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import { FirebaseContext } from "../context/firebase";
+import * as ROUTES from "../constants/routes";
 // useHistory allows users to push into different pageson an action
-import { useHistory } from 'react-router';
+import { useHistory } from "react-router";
 
 const Signin = () => {
-    const history = useHistory();
-    const { firebase } = useContext(FirebaseContext);
-    const [emailAddress, setEmailAddress] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const isInvalid = password === '' || emailAddress === '';
+  const isInvalid = password === "" || emailAddress === "";
 
-    const handleSignIn = (event) => {
-        event.preventDefault();
+  const handleSignIn = (event) => {
+    event.preventDefault();
 
-        // firebase auth(this is a Promise)
-        firebase
-            .auth()
-            .signInWithEmailAndPassword(emailAddress, password)
-            .then(() => {
-                // push to the browse page
-                history.push(ROUTES.BROWSE);
-            })
-            .catch((error) => {
-                setEmailAddress('');
-                setPassword('');
-                setError(error.message);
-            }); 
-    };
+    // firebase auth(this is a Promise)
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        // push to the browse page
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
 
-    return (
-        <>
-            <HeaderContainer>
-                <Form>
-                    <Form.Title>Sign In</Form.Title>
-                    {error && <Form.Error>{error}</Form.Error>}
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
 
-                    <Form.Base onSubmit={handleSignIn} method="POST">
-                        <Form.Input
-                            placeholder="Email Address"
-                            value={emailAddress}
-                            onChange={({ target }) => setEmailAddress(target.value)}
-                        />
-                        <Form.Input
-                            type="password"
-                            placeholder="Password"
-                            autoComplete="off"
-                            value={password}
-                            onChange={({ target }) => setPassword(target.value)}
-                        />
-                        <Form.Submit disabled={isInvalid} type="submit">
-                            Sign In
-                        </Form.Submit>
-                    </Form.Base>
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
 
-                    <Form.Text>
-                        New to Netflix? <Form.Link to="/signup">Sign up Now</Form.Link>
-                    </Form.Text>
-                    <Form.TextSmall>
-                        This page is protected by Google reCAPTCHA to ensure
-                        you're not a bot.
-                        <Form.Link to="#" > Learn more.</Form.Link>
-                    </Form.TextSmall>
-                </Form>
-            </HeaderContainer>
-            <FooterContainer />
-        </>
-    )
-}
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up Now</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot.
+            <Form.Link to="#"> Learn more.</Form.Link>
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
+};
 
-export default Signin
+export default Signin;
